6d13a04e24a65f73e44809a551eb9b69d20f8eec
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-07 18:29:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
 
-export default defineConfig(({ mode }) => ({
-  // Change this from "./" to "/"
-  base: "/", 
+export default defineConfig({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
-      // Ensure this matches your folder structure
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-}));
+})
