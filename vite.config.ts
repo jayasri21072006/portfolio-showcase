d8ad95691b9ea4dbc3ac2871fd015a6d4c40a4ef
jayasri21072006 <143895835+jayasri21072006@users.noreply.github.com>
2026-01-07 19:00:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",   // ⭐ THIS IS THE KEY FIX
+  // Use the name of your GitHub repository
+  // If your URL is https://username.github.io/portfolio/, set this to '/portfolio/'
+  base: "portfolio-showcase", 
+  
   plugins: [react()],
   resolve: {
     alias: {
